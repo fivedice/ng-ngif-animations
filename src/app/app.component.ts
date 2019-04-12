0520d30bce7281d60351c63eec508610dcdf9532
Jared Youtsey <jkyoutsey@bitbucket.org>
2019-04-11 20:21:47 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
+	animations: [
+		trigger('inOutAnimation', [
+			transition(':enter', [
+				style({ height: 0, opacity: 0 }),
+				animate('1s ease-out', style({ height: 300, opacity: 1 }))
+			]),
+			transition(':leave', [
+				style({ height: 300, opacity: 1 }),
+				animate('1s ease-in', style({ height: 0, opacity: 0 }))
+			])
+		])
+	]
 })
 export class AppComponent {
-  title = 'ngifAnimation';
+	title = 'ngifAnimation';
+
+	showImage = false;
+
+	onClick() {
+		this.showImage = !this.showImage;
+	}
 }
